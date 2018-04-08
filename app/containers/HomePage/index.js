90e7f9161bfc598312167a873275e3713953014b
justinnunez1717 <justinnunez1717@gmail.com>
2018-04-08 00:45:48 -0500

    Starting to change the HomePage UI. This will likely be scrapped.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -13,8 +13,9 @@ import { compose } from 'redux';
 import { push } from 'react-router-redux';
 
 import styled from 'styled-components';
-import BigBtn1 from 'components/BTNS/BigBtn1';
+// import BigBtn1 from 'components/BTNS/BigBtn1';
 import { getActive } from 'themes';
+import Form from 'components/Form/index';
 import PageHeader from 'components/PageHeader/index';
 
 import injectSaga from 'utils/injectSaga';
@@ -22,18 +23,47 @@ import injectReducer from 'utils/injectReducer';
 import makeSelectHomePage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
+import { getEntry } from './actions';
+import { SCHEMAS } from '../App/constants';
 import messages from './messages';
 
 export class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
     this.navigate = this.navigate.bind(this);
+    this.state = { selection: 'blank' };
+  }
+  changeSelection = (e) => {
+    this.setState({ selection: e.target.value });
   }
   navigate(path) {
     this.props.dispatch(push(path));
   }
+  submit(entryType) {
+    // NOTE: There is a conditional in the loop to check if name is defined
+    //       this can be removed once the definitions of the models are transfered
+    //       it is only there to prevent the models that are not connected from
+    //       throwing errors
+
+    const fields = document.getElementById('entryData').children;
+    const data = {};
+    for (let i = 0; i < fields.length; i += 1) {
+      if (fields[i].name === undefined || fields[i].name === '') {
+        window.alert('This entry has not defined connection to database');
+        return;
+      }
+      data[fields[i].name] = fields[i].value;
+    }
+    this.props.dispatch(getEntry(entryType, data));
+  }
   render() {
     const theme = getActive();
+    const Select = styled.select`
+      border: 1px solid black;
+      margin-left: 5px;
+      background-color: ${theme.get('primary')};
+      margin-bottom: 5px;
+    `;
     const FlexBox = styled.div`
       display: -webkit-flex;
       display: flex;
@@ -49,9 +79,14 @@ export class HomePage extends React.Component { // eslint-disable-line react/pre
         <PageHeader title={messages.header.defaultMessage} />
         <br />
         <FlexBox >
-          <BigBtn1 tab="1%" theme={theme} text="View Database" link="/database" handler={(link) => this.navigate(link)} />
-          <BigBtn1 tab="6%" theme={theme} text="Add/Delete Entries" link="/entries" handler={(link) => this.navigate(link)} />
-          <BigBtn1 tab="11%" theme={theme} text="Analysis" link="/analysis" handler={(link) => this.navigate(link)} />
+          <Select onChange={this.changeSelection}>
+            <option value="all">All</option>
+            <option value="target">Target</option>
+            <option value="experiment">Experiment</option>
+            <option value="chemical">Chemical</option>
+            <option value="toxicity">Toxicity</option>
+          </Select>
+          <Form schema={SCHEMAS[this.state.selection]} schemeType={this.state.selection} handler={this.submit} />
         </FlexBox>
       </div>
     );
